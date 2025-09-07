5451223bc1b9dadbc0936dc98d3d41c211c0ae48
Mahesh0703 <maheshbisurkar1985@gmail.com>
2025-09-07 19:36:46 +0530

    FMA-0001- Adding pagination and logs

--- food/src/main/java/com/food/service/dealer/DealerServiceImpl.java
+++ food/src/main/java/com/food/service/dealer/DealerServiceImpl.java
@@ -5,6 +5,8 @@ import com.food.repository.DealerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class DealerServiceImpl implements DealerService {
 
@@ -12,8 +14,14 @@ public class DealerServiceImpl implements DealerService {
     DealerRepository dealerRepository;
     @Override
     public Dealer insertDealer(String userId, Dealer dealer) {
-        dealer.setDealerInsertedBy(userId);
-        return dealerRepository.save(dealer);
+
+        if(Objects.isNull(dealer) || Objects.nonNull(dealer)){
+            dealer.setDealerInsertedBy(userId);
+            return dealerRepository.save(dealer);
+        }else {
+            throw new IllegalArgumentException("Dealer object is null");
+        }
+
 
     }
 }
